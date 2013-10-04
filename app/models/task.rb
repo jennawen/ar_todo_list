437f74fd94ac47792425d1528912cd3fc590016f
JonathanTR <Jonathan.d.reilly@gmail.com>
2013-10-04 16:29:09 -0700

    all done!

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
+require_relative '../../config/application'
+
 class Task < ActiveRecord::Base
-  
+  def to_s
+    "#{self.id}. #{self.description} #{self.completed ? ['X'] : [ ]}"
+  end
 end
--- todo.rb
+++ todo.rb
@@ -1,3 +1,37 @@
 require_relative 'config/application'
 
 puts "Put your application code in #{File.expand_path(__FILE__)}"
+
+user_input = ARGV
+
+def command_parser (user_input)
+
+  command = user_input.shift.downcase
+  body = user_input.join(' ')
+  update = "To do list has been updated"
+
+  case command
+
+  when "list"
+    Viewer.print(Task.all)
+  when "add"
+    Task.create(:description => body)  
+    Viewer.print(update)
+  when "delete"
+    Task.find(body.to_i).destroy
+    Viewer.print(update)
+  when "complete"
+    completed_task = Task.find(body.to_i)
+    completed_task.completed = true
+    completed_task.save
+    Viewer.print(update)
+  end
+end
+
+class Viewer
+  def self.print(to_be_printed)
+    puts to_be_printed
+  end
+end
+
+command_parser(user_input)
